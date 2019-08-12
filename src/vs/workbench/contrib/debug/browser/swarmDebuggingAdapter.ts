dbf9500d813530c5d4a7ddae4647eb5f9baff054
Vincent Gagnon <vincentgagnon53@gmail.com>
2019-08-12 17:17:05 -0400

    Started working on the files that are used to retrieve event information

---
+++ src/vs/workbench/contrib/debug/browser/swarmDebuggingAdapter.ts
@@ -0,0 +1,93 @@
+// tslint:disable-next-line: import-patterns
+import { request } from 'graphql-request';
+
+var currentSessionId = -1;
+const SERVER_URL = 'http://localhost:8080/graphql?';
+
+class Invocation {
+
+	invoking: Method;
+	invoked: Method;
+	session: number;
+
+	constructor(invoked: Method){
+		this.invoked = invoked;
+	}
+
+}
+
+class Method {
+
+	name: string;
+	type: string;
+
+	constructor(name: string, type:string){
+		this.name = name;
+		this.type = type;
+	}
+
+}
+
+class SwarmDebugEvent {
+
+	method: Method;
+	session: number;
+	lineNumber: number;
+	kind: string; //ex: stepIn, stepOut, ...
+
+	constructor(method: Method, lineNumber: number, kind: string) {
+		this.method = method;
+		this.lineNumber = lineNumber;
+		this.kind = kind;
+	}
+
+}
+
+function getEventInfo(response: DebugProtocol.Response, lastCalledFunction?: string) {
+
+	//sort out the commands
+	switch(response.command) {
+		case 'stepIn': {
+			getStepInInfo(response, lastCalledFunction);
+			break;
+		}
+		case 'stepOut': {
+
+			break;
+		} //add more cases
+		default: {
+			return;
+		}
+	}
+
+}
+
+//lastcalledid in server
+function getStepInInfo(response: DebugProtocol.Response, lastCalledFunction?: string) {
+
+	let invoked = new Method(response.body.stackFrames[0].name, response.body.stackFrames[0].source.path);
+
+	let event = new SwarmDebugEvent(invoked, response.body.stackFrames[0].line, 'Step Into');
+
+	let invocation = new Invocation(invoked);
+	if(lastCalledFunction){
+	//	invocation.invoking = new Method(lastCalledFunction);
+	}
+
+
+
+
+}
+
+//Where does this belong?
+function sendMethod(method: Method) {
+
+	//const querySession =
+
+	//const queryType = `mutation `
+
+	//const query = `mutation methodCreate($name: String, $type: string)`;
+
+
+
+}
