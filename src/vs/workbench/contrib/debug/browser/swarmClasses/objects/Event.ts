cbc1b780c0c2ac5e9b479e4acb6f4a125a745c86
iagored <iago.gc.eliote@gmail.com>
2019-08-14 05:19:16 -0400

    EventService created

--- src/vs/workbench/contrib/debug/browser/swarmClasses/objects/Event.ts
+++ src/vs/workbench/contrib/debug/browser/swarmClasses/objects/Event.ts
@@ -1,15 +1,43 @@
 import { Method } from '../objects/Method';
+import { Session } from '../objects/Session';
 
 export class Event {
 
+	id: number;
 	method: Method;
-	session: number;
+	session: Session;
 	lineNumber: number;
 	kind: string; //ex: stepIn, stepOut, ...
 
-	constructor(method: Method, lineNumber: number, kind: string) {
+	constructor(method: Method, session: Session, lineNumber: number, kind: string) {
 		this.method = method;
+		this.session = session;
 		this.lineNumber = lineNumber;
 		this.kind = kind;
 	}
+
+	getID() {
+		return this.id;
+	}
+
+	setID(id: number) {
+		this.id = id;
+	}
+
+	getMethod() {
+		return this.method;
+	}
+
+	getSession() {
+		return this.session;
+	}
+
+	getLineNumber() {
+		return this.lineNumber;
+	}
+
+	getKind() {
+		return this.kind;
+	}
+
 }
---
+++ src/vs/workbench/contrib/debug/browser/swarmClasses/services/eventService.ts
@@ -0,0 +1,57 @@
+// tslint:disable-next-line: import-patterns
+import { request } from 'graphql-request';
+import { SERVERURL } from '../../swarmAdapter';
+import { Event } from '../objects/Event';
+
+export class EventService {
+	event: Event | undefined;
+
+	constructor(event?: Event) {
+		this.event = event;
+	}
+
+	setEvent(event: Event) {
+		this.event = event;
+	}
+
+	async create() {
+		if (this.event) {
+			const query = `
+			mutation eventCreate(
+				$sessionId: Long,
+				$eventLineNumber: Long,
+				$eventKind: String,
+				$methodId: Long
+			){
+				eventCreate(
+				  event: {
+					session: {
+					  id: $sessionId
+					},
+					lineNumber: $eventLineNumber,
+					kind: $eventKind,
+					method: {
+						id: $methodId
+					}
+				  }
+				){
+				  id
+				}
+			  }`;
+
+			const variables = {
+				sessionId: this.event.getSession().getID(),
+				eventLineNumber: this.event.getLineNumber(),
+				eventKind: this.event.getKind(),
+				methodId: this.event.getMethod().getID()
+			};
+
+			let data = await request(SERVERURL, query, variables);
+			this.event.setID(data.eventCreate.id);
+
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
