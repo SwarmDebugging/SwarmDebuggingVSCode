65826e08116ac9ee83e3fa030aa46b22a9dc14cb
Vincent Gagnon <vincentgagnon53@gmail.com>
2019-08-17 14:48:28 -0400

    Added a switch to make the code cleaner and started implementing source command handling

--- src/vs/workbench/contrib/debug/browser/swarmAdapter.ts
+++ src/vs/workbench/contrib/debug/browser/swarmAdapter.ts
@@ -23,6 +23,8 @@ export class SwarmAdapter {
 	private lastSteppedInMethod: Method;
 	private lastSteppedInType: Type;
 	private lastSteppedInEvent: Event;
+	private continued: boolean = false;
+	private fileInNodeInternals = false;
 
 	// Data to persist
 	private vscodeSession: string;
@@ -57,133 +59,101 @@ export class SwarmAdapter {
 
 	async tryPersist(response: DebugProtocol.Response) {
 
-		if (response.command === 'variables' && this.workspace === false) {
-			this.rootPathInvoking = response.body.variables[1].value;
-			this.rootPathInvoking = this.rootPathInvoking.slice(1, this.rootPathInvoking.length - 1);
+		switch(response.command) {
+			case 'variables':
+				this.defineWorkspace(response);
+				break;
+			case 'stepIn':
+				this.steppedIn = true;
+				this.eventKind = 'stepIn';
+				break;
+			case 'stepOut':
+				this.eventKind = 'stepOut';
+				this.swarmEventService.setEvent(new Event(this.lastSteppedInMethod,
+					this.swarmSession,
+					this.lastSteppedInEvent.getLineNumber(),
+					this.eventKind));
+				await this.swarmEventService.create();
+				break;
+			case 'stackTrace':
+				if(this.secondStackTrace) {
+					this.steppedInSecondStackTrace(response);
+				} else if(this.steppedIn) {
+					this.steppedInFirstStackTrace(response);
+				}
+				break;
+			case 'source':
+				this.handleSourceCommand(response);
+				break;
+			case 'continue':
+				this.handleContinueCommand(response);
+				break;
+		}
+	}
+
+	handleContinueCommand(response: DebugProtocol.Response) {
+			//get current stop place and get function here
+			//what data to store???
+			//method = line where it was stopped
+			//in breakpoints????
+			console.log('continued');
+			this.continued = true;
+	}
+
+	handleSourceCommand(response: DebugProtocol.Response) {
+		if(this.steppedIn && this.secondStackTrace && this.fileInNodeInternals) {
+			let artefact = response.body.content;
+			this.swarmArtefactInvoked = new Artefact(artefact);
+			this.steppedIn = false;
+		} else if(this.continued) {
+			//save the source as new artefact
+
+			this.continued = false;
+		}
+	}
+
+	setSession(vscodeSessionId: string) {
+		this.vscodeSession = vscodeSessionId;
+	}
+
+	defineWorkspace(response: DebugProtocol.Response) {
+		if (this.workspace === false) {
+			this.rootPathInvoking = response.body.variables[1].value.slice(response.body.variables[1].value, response.body.variables[1].value.length - 1);
+			//this.rootPathInvoking = this.rootPathInvoking.slice(1, this.rootPathInvoking.length - 1);
 			this.workspace = true;
 		}
+	}
 
-		if (response.command === 'stepIn') {
-			this.steppedIn = true;
-			this.eventKind = 'stepIn';
+	async steppedInFirstStackTrace(response: DebugProtocol.Response) {
+		this.invoked = response.body.stackFrames[0].name;
+		// TO DO: Find the right path to access the file for the reading
+		/*i found out that <node_internals> are built-in core modules of Node.js
+		and i don't know how to access them which is a issue for the continue event
+		since i need the line in the file where the user is stopped*/
+		/*I found that in the source command in the body in the content attribute is the whole
+		content of the file to load, so we just need to associate this with the artfact we weren't able
+		to create before*/
+		let artefact;
+		try {
+			artefact = await fs.readFileSync(response.body.stackFrames[0].source.path, 'utf8');//error here
+			this.swarmArtefactInvoking = new Artefact(artefact);//isn't this invoked???*/
+			this.steppedIn = false;
+		} catch(error){
+			this.fileInNodeInternals = true;
+			console.log(error);
 		}
+		this.secondStackTrace = true;
+	}
 
-		if (response.command === 'stepOut') {
-			//this.steppedOut = true;
-			this.eventKind = 'stepOut';
+	async steppedInSecondStackTrace(response: DebugProtocol.Response) {
+		let result = await this.swarmSessionService.getByVscodeId(
+			this.vscodeSession
+		);
+		if (result instanceof Session) {
+			this.swarmSession = result;
 
+			this.invoking = response.body.stackFrames[0].name;
 
-			this.swarmEventService.setEvent(new Event(this.lastSteppedInMethod,
-				 this.swarmSession,
-				this.lastSteppedInEvent.getLineNumber(),
-				 this.eventKind));
-			await this.swarmEventService.create();
-
-			/*this.swarmMethodService.setMethod(this.lastSteppedInMethod);
-			await this.swarmMethodService.create();*/
-
-		}
-
-		/*if (response.command === 'stackTrace' && this.steppedOut) {
-
-			let result = await this.swarmSessionService.getByVscodeId(
-				this.vscodeSession
-			);
-			if (result instanceof Session && this.lastSteppedInMethod && this.lastSteppedInType) {
-
-				this.swarmSession = result;
-
-
-
-
-
-
-			}
-			this.steppedOut = false;
-		}*/
-
-		if (this.secondStackTrace && response.command === 'stackTrace') {
-
-			let result = await this.swarmSessionService.getByVscodeId(
-				this.vscodeSession
-			);
-			if (result instanceof Session) {
-				this.swarmSession = result;
-
-				this.invoking = response.body.stackFrames[0].name;
-
-				let artefact;
-				try {
-					artefact = await fs.readFileSync(response.body.stackFrames[0].source.path, 'utf8');//error here
-				} catch(error){
-					artefact = await '***' + response.body.stackFrames[0].source.origin + '***';
-					console.log(error);
-				}
-				this.swarmArtefactInvoking = new Artefact(artefact);
-
-				this.swarmTypeInvoking = new Type(
-					getTypeFullname(this.rootPathInvoking, response.body.stackFrames[0].source.path),
-					response.body.stackFrames[0].source.path,
-					fromPathToTypeName(response.body.stackFrames[0].source.path),
-					this.swarmArtefactInvoking,
-					this.swarmSession);
-
-				let createdTypes = await this.swarmTypeService.getAllBySession(this.swarmSession);
-				let shouldCreate = true;
-
-				for (let item of createdTypes) {
-					if (this.swarmTypeInvoking.equals(item)) {
-						this.swarmTypeInvoking.setID(item.getID());
-						shouldCreate = false;
-						break;
-					}
-				}
-				if (shouldCreate) {
-					this.swarmTypeService.setArtefact(this.swarmArtefactInvoking);
-					this.swarmTypeService.setType(this.swarmTypeInvoking);
-					let response = await this.swarmTypeService.create();
-					if (response) {
-						createdTypes[createdTypes.length] = this.swarmTypeInvoking;
-					}
-				}
-
-				this.swarmMethodInvoking = new Method(
-					this.invoking,
-					this.swarmTypeInvoking);
-				this.swarmMethodService.setMethod(this.swarmMethodInvoking);
-				await this.swarmMethodService.create();
-
-				this.swarmMethodInvoked = new Method(
-					this.invoked,
-					this.swarmTypeInvoking);
-				this.swarmMethodService.setMethod(this.swarmMethodInvoked);
-				await this.swarmMethodService.create();
-
-				let event = new Event(
-					//invoked or invoking?
-					this.swarmMethodInvoked,
-					this.swarmSession,
-					response.body.stackFrames[0].line,
-					this.eventKind
-				);
-				this.swarmEvent = event;
-				this.swarmEventService.setEvent(this.swarmEvent);
-				await this.swarmEventService.create();
-
-				this.swarmInvocation = new Invocation(this.swarmMethodInvoking, this.swarmMethodInvoked, this.swarmSession);
-				this.swarmInvocationService.setInvocation(this.swarmInvocation);
-				this.swarmInvocationService.create();
-
-				this.lastSteppedInMethod = this.swarmMethodInvoked;
-				this.lastSteppedInType = this.swarmTypeInvoked;
-				this.lastSteppedInEvent = event;
-			}
-			this.secondStackTrace = false;
-		}
-
-		if (this.steppedIn && response.command === 'stackTrace') {
-			this.invoked = response.body.stackFrames[0].name;
-			// TO DO: Find the right path to access the file for the reading
 			let artefact;
 			try {
 				artefact = await fs.readFileSync(response.body.stackFrames[0].source.path, 'utf8');//error here
@@ -192,13 +162,65 @@ export class SwarmAdapter {
 				console.log(error);
 			}
 			this.swarmArtefactInvoking = new Artefact(artefact);
-			this.steppedIn = false;
-			this.secondStackTrace = true;
-		}
-	}
 
-	setSession(vscodeSessionId: string) {
-		this.vscodeSession = vscodeSessionId;
+			this.swarmTypeInvoking = new Type(
+				getTypeFullname(this.rootPathInvoking, response.body.stackFrames[0].source.path),
+				response.body.stackFrames[0].source.path,
+				fromPathToTypeName(response.body.stackFrames[0].source.path),
+				this.swarmArtefactInvoking,
+				this.swarmSession);
+
+			let createdTypes = await this.swarmTypeService.getAllBySession(this.swarmSession);
+			let shouldCreate = true;
+
+			for (let item of createdTypes) {
+				if (this.swarmTypeInvoking.equals(item)) {
+					this.swarmTypeInvoking.setID(item.getID());
+					shouldCreate = false;
+					break;
+				}
+			}
+			if (shouldCreate) {
+				this.swarmTypeService.setArtefact(this.swarmArtefactInvoking);
+				this.swarmTypeService.setType(this.swarmTypeInvoking);
+				let response = await this.swarmTypeService.create();
+				if (response) {
+					createdTypes[createdTypes.length] = this.swarmTypeInvoking;
+				}
+			}
+
+			this.swarmMethodInvoking = new Method(
+				this.invoking,
+				this.swarmTypeInvoking);
+			this.swarmMethodService.setMethod(this.swarmMethodInvoking);
+			await this.swarmMethodService.create();
+
+			this.swarmMethodInvoked = new Method(
+				this.invoked,
+				this.swarmTypeInvoking);
+			this.swarmMethodService.setMethod(this.swarmMethodInvoked);
+			await this.swarmMethodService.create();
+
+			let event = new Event(
+				//invoked or invoking?
+				this.swarmMethodInvoked,
+				this.swarmSession,
+				response.body.stackFrames[0].line,
+				this.eventKind
+			);
+			this.swarmEvent = event;
+			this.swarmEventService.setEvent(this.swarmEvent);
+			await this.swarmEventService.create();
+
+			this.swarmInvocation = new Invocation(this.swarmMethodInvoking, this.swarmMethodInvoked, this.swarmSession);
+			this.swarmInvocationService.setInvocation(this.swarmInvocation);
+			this.swarmInvocationService.create();
+
+			this.lastSteppedInMethod = this.swarmMethodInvoked;
+			this.lastSteppedInType = this.swarmTypeInvoked;
+			this.lastSteppedInEvent = event;
+		}
+		this.secondStackTrace = false;
 	}
 
 }
